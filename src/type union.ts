2dfcc78ccb25d8f3cd9ec0236ecbf54f7705a36f
unknown <duvierreinosa@gmail.com>
2021-11-26 17:59:31 -0500

    union de tipos

---
+++ src/type union.ts
@@ -0,0 +1,20 @@
+//union de tipos
+
+let idUser: number | string;
+
+idUser = 10;
+idUser ='10';
+
+function getUsernameById(id: number | string) {
+    //lLOGIC THAN FIN USER
+    return 'DUVIER'
+}
+
+getUsernameById(10);
+getUsernameById('10');
+
+//tipos laterales
+//100x100, 500x500, 1000x1000
+type SquareSize = '100x100' | '500x500' | '1000x1000';
+//let smallPicture: SquareSize = '200x200'; error
+let smallPicture: SquareSize = '100x100';
